3782f171ca824fc12c327ea038ab4a102e979a18
Tess Austin <tessaustin92@gmail.com>
2021-06-15 16:31:16 -0600

    test actions and reducers successful

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -31,3 +31,7 @@ export const reducer = (state, action) => {
             return state;
     }
 };
+
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+}
