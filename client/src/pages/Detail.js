b38748dff05be75a10f2701587e1323afbd0db37
Tess Austin <tessaustin92@gmail.com>
2021-06-16 11:18:34 -0600

    implement adding items to the cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,11 +3,17 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 import Cart from '../components/Cart';
+
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -30,6 +36,13 @@ function Detail() {
     }
   }, [products, data, dispatch, id]);
 
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
+
   return (
     <>
       {currentProduct ? (
@@ -48,8 +61,8 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
-              Add to Cart
+            <button 
+            onClick={addToCart}>Add to cart
             </button>
             <button>
               Remove from Cart
