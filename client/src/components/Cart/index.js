a18b994aee0735d0039dfaee1dd314d3ffb7d616
Tess Austin <tessaustin92@gmail.com>
2021-06-16 19:30:48 -0600

    redirect to Stripe Checkout on the front end

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
-import CartItem from '../CartItem';
-import Auth from '../../utils/auth';
-import './style.css';
-
-import { useStoreContext } from '../../utils/GlobalState';
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from '@apollo/react-hooks';
+import { QUERY_CHECKOUT } from "../../utils/queries"
+import { idbPromise } from "../../utils/helpers"
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import "./style.css";
 
-import { idbPromise } from "../../utils/helpers";
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            })
+        }
+    }, [data]);
 
     useEffect(() => {
         async function getCart() {
@@ -35,6 +46,20 @@ const Cart = () => {
         return sum.toFixed(2);
     }
 
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        });
+    }
+
     if (!state.cartOpen) {
         return (
             <div className="cart-closed" onClick={toggleCart}>
@@ -54,11 +79,13 @@ const Cart = () => {
                     {state.cart.map(item => (
                         <CartItem key={item._id} item={item} />
                     ))}
+
                     <div className="flex-row space-between">
                         <strong>Total: ${calculateTotal()}</strong>
+
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
